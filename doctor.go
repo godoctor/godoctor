d00a7823cc1b9dc23e8c7049cdd355326c3f89fa
Reed Allman <rdallman10@gmail.com>
2013-10-07 21:30:54 -0500

    add initial package structure

---
+++ doctor.go
@@ -0,0 +1,14 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package doctor
+
+/*
+  This should be where most of the magic happens.
+  In my humble opinion, I think this should just expose
+  one entry point to start the oracle with appropriate params.
+
+  e.g. the executable can be put elsewhere.
+*/
+
---
+++ doctor/main.go
@@ -0,0 +1,17 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+/*
+  Executable stuff goes here for command line
+*/
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println("Docta Docta, give me the news, I got a bad case of refactoring booze.")
+}
