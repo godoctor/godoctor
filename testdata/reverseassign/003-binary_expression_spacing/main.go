f03b07a88ff5357ae193d65936511ddba7cb7a8c
Jeffrey Overbey <com-github@jeff.over.bz>
2014-02-20 16:13:45 -0600

    Added Steffi's fix for revassign test 003 to prevent nil pointer deref

--- testdata/reverseassign/003-binary_expression_spacing/main.go
+++ testdata/reverseassign/003-binary_expression_spacing/main.go
@@ -1,11 +1,11 @@
-// <<<<< reverseassign,8,3,8,10,fail
+// <<<<< reverseassign,8,3,8,21,fail
 package main
 
 import "fmt"
 import "reflect"
 
 func main() {
-  var i int=3+4.5
+  var i float64=3+4.5
   fmt.Println("type of i is", reflect.TypeOf(i))
   fmt.Println("value of i is", i)
 }
