73cf79a629a31a79007a478807c82f55ece3095b
Jeffrey Overbey <com-github@jeff.over.bz>
2014-09-19 15:11:40 -0500

    Inlined comment search into FindComments

--- analysis/names/comments.go
+++ analysis/names/comments.go
@@ -19,29 +19,21 @@ import (
 func FindInComments(name string, f *ast.File, fset *token.FileSet) []text.Extent {
 	result := []text.Extent{}
 	for _, comment := range f.Comments {
-		if strings.Contains(comment.List[0].Text, name) {
-			result = append(result, occurrences(name, comment, fset)...)
+		slashIdx := fset.Position(comment.List[0].Slash).Offset
+		whitespaceIdx := 1
+		regexpstring := fmt.Sprintf("[\\PL]%s[\\PL]|//%s[\\PL]|/\\*%s[\\PL]|[\\PL]%s$", name, name, name, name)
+		re := regexp.MustCompile(regexpstring)
+		matchcount := strings.Count(comment.List[0].Text, name)
+		for _, idx := range re.FindAllStringIndex(comment.List[0].Text, matchcount) {
+			var offset int
+			if idx[0] == 0 {
+				offset = slashIdx + idx[0] + whitespaceIdx + 1
+			} else {
+				offset = slashIdx + idx[0] + whitespaceIdx
+			}
+			result = append(result, text.Extent{offset, len(name)})
 		}
-	}
-	return result
-}
 
-// occurrences returns the source location of the given name in the given
-// CommentGroup.
-func occurrences(name string, comment *ast.CommentGroup, fset *token.FileSet) []text.Extent {
-	var result []text.Extent
-	whitespaceindex := 1
-	regexpstring := fmt.Sprintf("[\\PL]%s[\\PL]|//%s[\\PL]|/\\*%s[\\PL]|[\\PL]%s$", name, name, name, name)
-	re := regexp.MustCompile(regexpstring)
-	matchcount := strings.Count(comment.List[0].Text, name)
-	for _, matchindex := range re.FindAllStringIndex(comment.List[0].Text, matchcount) {
-		var offset int
-		if matchindex[0] == 0 {
-			offset = fset.Position(comment.List[0].Slash).Offset + matchindex[0] + whitespaceindex + 1
-		} else {
-			offset = fset.Position(comment.List[0].Slash).Offset + matchindex[0] + whitespaceindex
-		}
-		result = append(result, text.Extent{offset, len(name)})
 	}
 	return result
 }
