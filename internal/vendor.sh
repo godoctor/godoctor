8c2c9b061f2322533d86758c1471804252dcde15
Jeffrey Overbey <com-github@jeff.over.bz>
2015-07-08 09:28:32 -0600

    Removed releng; updated vendoring script

--- internal/vendor.sh
+++ internal/vendor.sh
@@ -3,20 +3,20 @@
 # Vendoring of third-party packages for the Go Doctor
 
 if [ `dirname $0` != '.' ]; then
-	echo "vendor.sh must be run with releng as the current directory"
+	echo "vendor.sh must be run from the internal directory"
 	exit 1
 fi
 
 # Change to the root of the godoctor repository
 cd ..
 
-FILE=`pwd`/internal/versions.txt
+FILE=`pwd`/versions.txt
 
 echo "Logging versions to $FILE..."
 date >$FILE
 for pkg in golang.org/x/tools github.com/cheggaaa/pb github.com/willf/bitset; do
 	pushd . >/dev/null
-	cd internal/$pkg
+	cd $pkg
 	echo "" >>$FILE
 	echo "$pkg" >>$FILE
 	git remote -v | head -1 >>$FILE
@@ -27,16 +27,19 @@ done
 
 echo "Removing unused portions of go.tools..."
 pushd . >/dev/null
-cd internal/golang.org/x/tools && rm -rf blog cmd cover dashboard godoc imports oracle playground present refactor codereview.cfg go/callgraph go/gccgoimporter go/importer go/pointer go/vcs
+cd golang.org/x/tools && rm -rf blog cmd cover dashboard godoc imports oracle playground present refactor codereview.cfg go/callgraph go/gccgoimporter go/importer go/pointer go/vcs
 popd >/dev/null
 
 echo "Removing tests from third-party packages..."
-find internal -iname '*_test.go' -delete
+find . -iname '*_test.go' -delete
 
 echo "Rewriting import paths in Go Doctor and third-party sources..."
+pushd . >/dev/null
+cd ..
 find . -iname '*.go' -exec sed -e 's/"golang.org\/x\//"github.com\/godoctor\/godoctor\/internal\/golang.org\/x\//g' -i '' '{}' ';'
 find . -iname '*.go' -exec sed -e 's/"github.com\/cheggaaa\//"github.com\/godoctor\/godoctor\/internal\/github.com\/cheggaaa\//g' -i '' '{}' ';'
 find . -iname '*.go' -exec sed -e 's/"github.com\/willf\//"github.com\/godoctor\/godoctor\/internal\/github.com\/willf\//g' -i '' '{}' ';'
+popd >/dev/null
 
 echo "DONE"
 exit 0
