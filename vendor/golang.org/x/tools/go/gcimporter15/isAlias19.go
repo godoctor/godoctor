fdfd618c750ea7b41e22f2d13476f6dfe2ea9ab9
Jeffrey Overbey <com-github@jeff.over.bz>
2017-05-07 17:29:16 -0400

    Vendor for Go 1.8

--- vendor/golang.org/x/tools/go/gcimporter15/isAlias19.go
+++ vendor/golang.org/x/tools/go/gcimporter15/isAlias19.go
@@ -1,13 +1,13 @@
-// Copyright 2016 The Go Authors. All rights reserved.
+// Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.6
+// +build go1.9
 
 package gcimporter
 
 import "go/types"
 
-func setName(pkg *types.Package, name string) {
-	pkg.SetName(name)
+func isAlias(obj *types.TypeName) bool {
+	return obj.IsAlias()
 }
