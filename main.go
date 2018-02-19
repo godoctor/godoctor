acb8df5c0053dceb9f37b235ad38c231e2736576
Jeffrey Overbey <com-github@jeff.over.bz>
2018-02-19 08:42:23 -0500

    Update version number to 0.6

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Auburn University and others. All rights reserved.
+// Copyright 2017-2018 Auburn University and others. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -17,8 +17,8 @@ import (
 var name string = "Go Doctor"
 
 // Go Doctor version number.  This can be overridden using:
-// go build -ldflags "-X main.version 0.5" github.com/godoctor/godoctor
-var version string = "0.5 (Beta)"
+// go build -ldflags "-X main.version 0.6" github.com/godoctor/godoctor
+var version string = "0.6 (Beta)"
 
 func main() {
 	aboutText := fmt.Sprintf("%s %s", name, version)
